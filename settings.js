2ae303475e8f36eb0f3f448958c43e242c038eb8
⋆˚｡⋆୨✧୧˚ 𝑴𝒓 𝒘𝒊𝒔𝒆 ˚୨✧୧⋆｡˚⋆ <benardwachira49@gmail.com>
2025-06-13 11:01:41 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'SNOWBIRD',
+  author: '‎',
+  botName: "⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛𝗦𝗡𝗢𝗪𝗕𝗜𝗥𝗗-𝗫𝗠𝗗 𝗩2",
+  botOwner: 'SNOWBIRD', // Your name
+  ownerNumber: '263780145644', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.0",
+};
+
+module.exports = settings;
